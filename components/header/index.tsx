7f825ca1cf630394dbd68e6f5ebd95004b809d6a
Sebastian Cuadros <cuadros@Sebastians-MBP.attlocal.net>
2023-03-04 15:20:31 -0500

    first commit

---
+++ components/header/index.tsx
@@ -0,0 +1,6 @@
+export default function Header() {
+    return <nav className="flex">
+        <div className="text-red-800"> Left </div>
+        <div className="text-lime-700"> Right </div>
+    </nav>
+}
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+import Header from "@/components/header";
+
+export default function LandingPage() {
+    return <div> <Header/> </div>;
+}
