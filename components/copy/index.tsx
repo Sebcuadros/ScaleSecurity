088bd70d67dd1e2c01f7493cd10a0ba7bb6a5a63
Sebastian Cuadros <cuadros@Sebastians-MBP.attlocal.net>
2023-03-05 17:55:02 -0500

    text size

--- components/copy/index.tsx
+++ components/copy/index.tsx
@@ -2,21 +2,21 @@ import Button from "../UI/button"
 
 export default function Copy() {
     return (
-        <div className="z-10 h-screen relative text-white overflow-scroll">
+        <div className="z-10 h-screen w-screen relative bg-black md:bg-transparent text-white overflow-scroll">
             <div className="h-1/2 text-center">
-                <h1 className="text-9xl mt-4">SCALE SECURITY</h1>
+                <h1 className="text-3xl md:text-9xl mt-4">SCALE SECURITY</h1>
             </div>
             <div className="h-3/4 text-left ml-5">
-                <p className="text-8xl">ScaleSecurity, a boutique Web3 security recruitment firm</p>
+                <p className="text-1xl md:text-8xl">ScaleSecurity, a boutique Web3 security recruitment firm</p>
             </div>
             <div className="h-1/4 text-left ml-5">
-                <p className="text-6xl">Research and engineering hires for the best teams building on Ethereum.</p>
+                <p className="text-1xl md:text-6xl">Research and engineering hires for the best teams building on Ethereum.</p>
             </div>
             <div className="h-1/4 text-left ml-5">
-                <p className="text-6xl">Technical recruiters that understand your products, problem sets, and needs. </p>
+                <p className="text-1xl md:text-6xl">Technical recruiters that understand your products, problem sets, and needs. </p>
             </div>
             <div className="h-1/6 text-left ml-5">
-                <p className="text-6xl">Chat with us below!</p>
+                <p className="text-1xl md:text-6xl">Chat with us below!</p>
             </div>
             <div>
                < Button/>
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -3,8 +3,8 @@ import ScrollHeader from "../UI/scrollHeader"
 
 export default function Header() {
     return <nav className="flex w-screen bg-black">
-        <div className="basis-1/2"> Left </div>
+        <div className="basis-1/6 md:basis-1/2"> </div>
 
-        <div className="basis-1/2 flex justify-end mr-4"> <ScrollHeader/>< Button/> </div>
+        <div className="basis-5/6 md:basis-1/2 flex justify-end mr-4"> <ScrollHeader/>< Button/> </div>
     </nav>
 }
