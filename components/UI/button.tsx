c86c2d205c106350179e2d9b9d698f433d8858b1
Sebastian Cuadros <cuadros@Sebastians-MBP.attlocal.net>
2023-03-05 12:27:44 -0500

    email clicks

--- components/UI/button.tsx
+++ components/UI/button.tsx
@@ -1,5 +1,5 @@
 export default function Button() {
-    return <button className="text-lime-400 font-black hover:bg-lime-400 hover:text-white border-2 border-lime-400 rounded-lg py-3 px-3 ml-8 my-4">
+    return <button onClick={(e) => {window.location.href ='mailto:hello@scalesecurity.xyz';}} className="text-lime-400 font-black hover:bg-lime-400 hover:text-white border-2 border-lime-400 rounded-lg py-3 px-3 ml-8 my-4">
         HELLO@SCALESECURITY.XYZ
     </button>
 }
