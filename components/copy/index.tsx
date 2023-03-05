721e6d28514349e3259128147e9b357550c37381
Sebastian Cuadros <cuadros@Sebastians-MBP.attlocal.net>
2023-03-05 12:55:54 -0500

    copy fixes

--- components/copy/index.tsx
+++ components/copy/index.tsx
@@ -9,11 +9,14 @@ export default function Copy() {
             <div className="h-3/4 text-left ml-5">
                 <p className="text-8xl">ScaleSecurity, a boutique Web3 security recruitment firm</p>
             </div>
-            <div className="h-3/4 text-left ml-5">
-                <p className="text-6xl">Researches, auditors, engineers, quants, and more</p>
+            <div className="h-1/4 text-left ml-5">
+                <p className="text-6xl">Research and engineering hires for the best teams building on Ethereum.</p>
             </div>
             <div className="h-1/4 text-left ml-5">
                 <p className="text-6xl">Technical recruiters that understand your products, problem sets, and needs. </p>
+            </div>
+            <div className="h-1/6 text-left ml-5">
+                <p className="text-6xl">Chat with us below!</p>
             </div>
             <div>
                < Button/>
