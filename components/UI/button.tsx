0807c8f83a2b3134765f2dc041151c0eb987a41e
Sebastian Cuadros <cuadros@Sebastians-MBP.attlocal.net>
2023-03-06 16:38:57 -0500

    font changes

--- components/UI/button.tsx
+++ components/UI/button.tsx
@@ -1,5 +1,10 @@
 export default function Button() {
-    return <button onClick={(e) => {window.location.href ='mailto:hello@scalesecurity.xyz';}} className="text-lime-400 font-black hover:bg-lime-400 hover:text-white border-2 border-lime-400 rounded-lg py-3 px-3 ml-8 my-4">
+    return <div>
+    <button onClick={(e) => {window.location.href ='mailto:hello@scalesecurity.xyz';}} className="text-lime-400 hidden md:block md:font-black hover:bg-lime-400 hover:text-white border-2 border-lime-400 rounded-lg py-3 px-3 ml-8 my-4">
         HELLO@SCALESECURITY.XYZ
     </button>
+    <button onClick={(e) => {window.location.href ='mailto:hello@scalesecurity.xyz';}} className="text-lime-400 md:hidden md:font-black hover:bg-lime-400 hover:text-white border-2 border-lime-400 rounded-lg py-3 px-3 ml-8 my-4">
+        CONTACT
+    </button>
+    </div>
 }
--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import ScaleSecurityDark from "public/ScaleSecurityDark.png";
 
 export default function Hero() {
-    return <div className="z-0 absolute h-screen w-screen">
-            <Image src={ScaleSecurityDark} alt="Dragon protecting gold"/>
+    return <div className="z-0 absolute">
+            <Image className="h-screen" src={ScaleSecurityDark} alt="Dragon protecting gold"/>
             </div>
 }
