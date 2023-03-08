b798ee0742e29fa69947768aae873cc43588ed81
Seb <seb@dibseed.com>
2023-03-08 17:01:02 -0500

    bg changes

--- components/UI/button.tsx
+++ components/UI/button.tsx
@@ -1,5 +1,5 @@
 export default function Button() {
-    return <div>
+    return <div className="bg-neutral-200">
     <button onClick={(e) => {window.location.href ='mailto:hello@scalesecurity.xyz';}} className="text-lime-400 hidden md:block md:font-black hover:bg-lime-400 hover:text-white border-2 border-lime-400 rounded-lg py-3 my-4">
         HELLO@SCALESECURITY.XYZ
     </button>
