f2794af1d3253fe4135265fc045795ca2683c3ba
Seb <seb@dibseed.com>
2023-03-08 17:02:01 -0500

    font size changed for smallest phones

--- components/UI/button.tsx
+++ components/UI/button.tsx
@@ -3,7 +3,7 @@ export default function Button() {
     <button onClick={(e) => {window.location.href ='mailto:hello@scalesecurity.xyz';}} className="text-lime-400 hidden md:block md:font-black hover:bg-lime-400 hover:text-white border-2 border-lime-400 rounded-lg py-3 my-4">
         HELLO@SCALESECURITY.XYZ
     </button>
-    <button onClick={(e) => {window.location.href ='mailto:hello@scalesecurity.xyz';}} className="text-lime-400 md:hidden md:font-black text-2xl hover:bg-lime-400 hover:text-white border-2 border-lime-400 rounded-lg py-3 my-4">
+    <button onClick={(e) => {window.location.href ='mailto:hello@scalesecurity.xyz';}} className="text-lime-400 md:hidden md:font-black text-xl sm:text-2xl hover:bg-lime-400 hover:text-white border-2 border-lime-400 rounded-lg py-3 my-4">
         HELLO@SCALESECURITY.XYZ
     </button>
     </div>
