6e944cca6a0a57fa5b6f903d7afdab214677834f
Seb <seb@dibseed.com>
2023-03-08 12:47:58 -0500

    copy changes

--- components/copy/index.tsx
+++ components/copy/index.tsx
@@ -2,21 +2,26 @@ import Button2 from "../UI/button2"
 
 export default function Copy() {
     return (
-        <div className="z-10 h-screen w-screen relative bg-neutral-200 text-black overflow-scroll">
+        <div className="z-10 h-screen w-screen relative bg-neutral-200 text-black">
             <div className="text-center">
-                <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-black">SCALE SECURITY</h1>
+                <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-black">WEB3 SECURITY HIRES MADE EASY</h1>
             </div>
-            <div className="text-left ml-5 mb-4">
-                <p className="text-base sm:text-base md:text-xl lg:text-2xl 2xl:text-3xl font-bold">WEB3 SECURITY HIRES MADE EASY</p>
+            <div className="text-left ml-5 mb-24">
+                <p className="text-base sm:text-base md:text-xl lg:text-2xl 2xl:text-3xl font-semibold text-center mb-10">End to end recruitment support for teams building Web3 products</p>
             </div>
-            <div className="text-left ml-5 mb-4">
-                <p className="text-2xl md:text-5xl font-bold">Research and engineering hires for the best teams building Web3 products</p>
-            </div>
-            <div className="text-left ml-5 mb-4">
-                <p className="text-2xl md:text-5xl font-bold">Technical recruiters that understand your products, problem sets, and needs. </p>
-            </div>
-            <div className="text-left ml-5 mb-4">
-                <p className="text-2xl md:text-5xl font-bold">Start hiring faster.</p>
+            <div className="flex-row justify-center space-x-24">
+                <div className="text-center w-72 h-96 bg-neutral-100 px-10 rounded">
+                    <h2 className="text-2xl md:text-3xl font-extrabold mb-3"> Easy Onboarding </h2>
+                    <p className="text-xl md:text-2xl font-medium">Schedule a free consult session with our core team, walk us through your needs and you're good to go. </p>
+                </div>
+                <div className="text-center w-72 h-96 bg-neutral-100 px-10 rounded">
+                    <h2 className="text-2xl md:text-3xl font-extrabold mb-3"> Specialized Search </h2>
+                    <p className="text-xl md:text-2xl font-medium">Technical recruiters that can tech and culture align engineers and employers</p>
+                </div>
+                <div className="text-center w-72 h-96 bg-neutral-100 px-10 rounded">
+                    <h2 className="text-2xl md:text-3xl font-extrabold mb-3"> Custom Recruitment</h2>
+                    <p className="text-xl md:text-2xl font-medium">End to end recruitment support from sourcing, interviewing, onboarding, and tooling set up.</p>
+                </div>
             </div>
             <div className="mb-4 md:mb-28">
                < Button2/>
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -3,7 +3,7 @@ import ScrollHeader from "../UI/scrollHeader"
 
 export default function Header() {
     return <nav className="flex w-screen bg-black">
-        <div className="basis-1/6 md:basis-1/2"> </div>
+        <div className="basis-1/6 md:basis-1/2 text-left text-white ml-4 mt-4 font-black text-2xl">ScaleSecurity</div>
 
         <div className="basis-5/6 md:basis-1/2 flex justify-end mr-4"> <ScrollHeader/>< Button/> </div>
     </nav>
