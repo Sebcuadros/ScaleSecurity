f69885f9bd337aa1a8677e89f3331012c369dde2
Seb <seb@dibseed.com>
2023-03-09 15:28:25 -0500

    styling bg not going w

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -2,11 +2,11 @@ import Button from "../UI/button"
 import ScrollHeader from "../UI/scrollHeader"
 
 export default function Header() {
-    return <nav className="flex flex-wrap bg-black justify-center sm:justify-between">
-        <div className="text-white ml-2 md:ml-4 mr-2 mt-4 md:mt-7 hidden md:block font-black text-xl">ScaleSecurity</div>
+    return <nav className="flex flex-wrap w-screen bg-black justify-center sm:justify-between">
+        <div className="text-white ml-2 md:ml-4 pr-2 mt-4 md:mt-7 hidden md:block font-black text-xl">ScaleSecurity</div>
 
-        <div className="mr-1"> <ScrollHeader/></div>
+        <div className="pr-1"> <ScrollHeader/></div>
 
-        <div className="lg:mr-4 xl:mr-6 hidden md:block"><Button/></div>
+        <div className="lg:pr-4 xl:pr-6 hidden md:block"><Button/></div>
     </nav>
 }
