f6acfda52eb9b181b8dc9d71ad5a887c6c89bafa
Seb <seb@dibseed.com>
2023-03-09 15:23:52 -0500

    mobile overflow on scroll fixed

--- components/UI/scrollHeader.tsx
+++ components/UI/scrollHeader.tsx
@@ -5,8 +5,8 @@ export default function ScrollHeader() {
             <div className="scroll-element secondary overflow-hidden text-clip font-black text-neutral-100">&nbsp;Sourcing, Recruiting, and Tool setup for <span className="text-lime-300">Web3</span> orgs</div>
         </div>
         <div className="scroll-parent-mobile py-3 my-4 sm:hidden">
-            <p className="scroll-element primary text-clip font-black text-neutral-100 text-l">&nbsp;Sourcing, Recruiting, and Tool setup for <span className="text-lime-300">Web3</span> orgs</p>
-            <p className="scroll-element secondary text-clip font-black text-neutral-100 text-l">&nbsp;Sourcing, Recruiting, and Tool setup for <span className="text-lime-300">Web3</span> orgs</p>
+            <p className="scroll-element primary text-clip font-black overflow-hidden text-neutral-100 text-l">&nbsp;End to end <span className="text-lime-300">Web3</span> recruiting</p>
+            <p className="scroll-element secondary text-clip font-black overflow-hidden text-neutral-100 text-l">&nbsp;End to end <span className="text-lime-300">Web3</span> recruiting</p>
         </div>
     </div>
 }
